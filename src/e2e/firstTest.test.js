5bbc4899ed35c7604e1f39af06f0ca6d568aa8bd
angel <erodriguezfeliz@gmail.com>
2017-12-06 00:53:15 -0400

    working test

---
+++ src/e2e/firstTest.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Presentation from '../components/Presentation'
+import renderer from 'react-test-renderer'
+
+it('look first test', () => {
+  const component = renderer.create(<Presentation />)
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
