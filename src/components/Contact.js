35a5ed50ebb59f766596c6aa3397047ed7afbcf4
angel <erodriguezfeliz@gmail.com>
2018-01-08 23:46:12 -0400

    add some style rules

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,13 @@ const Contact = () => {
           <p>CHeck out!!</p>
         </div>
         <div className="contact-paragraph">
+          <p>
+            {`This is my portfolio so far I hoope you've liked it was created with
+            love and music. You can contact me through my social media networks
+            for any questions suggestions or any topics like sports photography
+            science innovation and projects to improve the quality of life of
+            humanity.`}
+          </p>
           <p>Hope to hear from you! Best regards.</p>
         </div>
 
