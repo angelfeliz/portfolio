9e0a0e6632456c152336f72318dbe6ab1e6d8d33
Angel Rodriguez <erodriguezfeliz@gmail.com>
2018-03-08 23:52:42 -0400

    fix: intro text

--- src/helpers/valueString.js
+++ src/helpers/valueString.js
@@ -2,13 +2,13 @@ export const valueString = {
   work_with_me: `Mindful of the deadlines and project guidelines. Proven competency working in a team, 
                  prove multicultural social skills working in an international company. Active member of 
                  the Chingu Developer community `,
-  about: `I start my developer career with the OOP paradigm,  my first job was working with C# 
-         as the years pass and I was always improving my learning in the .NET world. One day a
-         good friend of mine showed me the other face of javascript that I did not know. After that I start digging 
+  about: `I started my developer career with the OOP paradigm,  my first job was working with C# 
+         as the years went by and I improved my skills in the .NET. One day a
+         good friend showed me the other face of javascript that I did not know. After that I started learning 
          more in the JS world and learning about Functional Programming. I felt attached with FP and 
          JS I decided to pursue JS programming.
  
-  “Ever stop learning whatever you like” is something people say that I believe in greatly`,
+  “Never stop learning whatever you love" is something people say that I believe in greatly`,
   team: `Team spirit is a key for the success of a project or within the work space. Basic principles of soft skills, communication is one of my abilities to improve day-to-day relationships. As a member of a team, I always look for good interaction and integration.`,
   code: `I have 4 years of coding experiences. Learning and improving my coding skills are challenges where I do my best to overcome. Software community is growing fast that is why I’m always on top of  new libraries or frameworks, reading books and post, catching up with tweets, listening to podcast, and watching a few developers events.`
 }
