6a9c188ac54218a0527be2b6b3ba5d409bd58739
angel.rodriguez <angel.rodriguez@nagico.com>
2018-07-25 17:10:48 -0400

    feat: add detail

--- src/helpers/clientsProjectsDoc.js
+++ src/helpers/clientsProjectsDoc.js
@@ -1,5 +1,38 @@
-export default [
-  {
+export default {
+  en:[{
     name: 'Budget Planning System'
-  }
-]
+  }],
+  es: [
+        {
+          name: 'Sistema de Planificación Presupuestaria',
+          useCase: 'El proceso de gestión de presupuesto de la empresa X,'
+                  +' en su etapa de formulación, presentaba una capacidad'
+                  +' menor que la demanda a la que estaba sujeta, es decir'
+                  +' que existía un “cuello de botella”, debido a la'
+                  +' obligatoriedad de coordinar las actividades entre dos'
+                  +' o más áreas internas o departamentos de la empresa y'
+                  +' sin una herramienta de estandarización de registro de'
+                  +' la información de las necesidades de cada centro de'
+                  +' costo. El proceso de recolección de información presupuestal'
+                  +' anual se ejecutaba a través de hojas de Excel que un'
+                  +' usuario aprobador debía consolidar en un presupuesto'
+                  +' general de la empresa corrigiendo: nombres incorrectos de'
+                  +' ítems de gastos, activos, cuentas contables, nombres de'
+                  +' personal, ortografía entre otros errores. Este proceso tenía una'
+                  +' duración aproximada de un mes y medio para ser aprobado el'
+                  +' presupuesto anual de la empresa. Cabe destacar que toda'
+                  +' herramienta tecnológica debe servir de soporte para una tarea,'
+                  +' proceso o procedimiento, por lo que la aplicación desarrollada'
+                  +' apostaba por la automatización del registro de la información'
+                  +' presupuestaria a través de catálogo de ítems, activos, personal,'
+                  +' ventas, mantenimientos asociados a las cuentas contables por'
+                  +' centro de costo o departamentos así como también la integridad y'
+                  +' calidad de los datos registrados que permiten la confección de'
+                  +' un presupuesto anual empresarial que puede ser aprobado y'
+                  +' consultado a través de gráficas y reportes. El tiempo estimado'
+                  +' de duración del proceso de formulación presupuestaria a través'
+                  +' del Sistema de Planificación Presupuestaria se estima en menos de 15 días:'
+        }
+    ]
+} 
+
