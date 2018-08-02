c02b676da1f89469610a8ac64d2fbc2bdafbc57f
angel.rodriguez <angel.rodriguez@nagico.com>
2018-08-02 14:12:08 -0400

    feat: translate use case

--- src/helpers/clientsProjectsDoc.js
+++ src/helpers/clientsProjectsDoc.js
@@ -6,13 +6,24 @@ export default {
                   +' manual constructor stage, had a lower capacity than the'
                   +' demand to which it was addressed,'
                   +' in other words, there was a "bottleneck"'
-                  +' due of the mandatory coordination of the paper work activity that required the process in'
-                  +' all the departments in company.'
-                  +' The process flow is gathering the information for the anual budget is execute through excel sheet in each department,'
-                  +' which then all the excel documents are delivered to one persone for there approve and consolidate in  , '
+                  +' due of the mandatory coordination of the paper work'
+                  +' activity that required the process in all the'
+                  +' departments in the company. The process flow gather'
+                  +' the information for the anual budget through a'
+                  +' excel sheet in each department, which then all the'
+                  +' excel documents are delivered to one persone for'
+                  +' the correct approve and consolidate to the anual'
+                  +' budget of the company, in the course of the approve'
+                  +' process as it is manual the person have to fix incorrect'
+                  +' name of the expense items, assets, accounting accounts'
+                  +' and typo name of the employees. This process duration'
+                  +' was a month and half to be approve as valid anual budget. \n'
+                  +' It should be noted that the software solution, implemented,'
+                  +' fixed the manuel process and reduce the working'
+                  +' time in more than the half. Solution automaize the register of'
+                  +' budgetary information through '
                   
-                  El proceso de recolección de información presupuestal anual se ejecutaba a través de hojas de Excel 
-                  que un usuario aprobador debía consolidar en un presupuesto general de la empresa corrigiendo:
+                  
         }
   ],
   es: [
